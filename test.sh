bec3e7d866dc42d9da12b8955318194e8e5ce90e
Greg Ingelmo <git@ingelmo.org>
2015-04-01 12:12:34 -0400

    Strip test output of dirs w/o tests

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # go test -v ./... 2>&1 | sed "s/.* assertion.*/[0m/" | grep -v -E "^(-|$|\?|PASS|ok)"
-go test ./... 
+
+# go test -v ./... -run CreateListMsg
+go test ./...  | grep -iEv "^\?"
