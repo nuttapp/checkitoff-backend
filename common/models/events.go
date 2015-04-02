07249877d8f8fc04a7c043e5f2f84bc1cc7750a4
Greg Ingelmo <git@ingelmo.org>
2015-04-02 00:45:30 -0400

    Add test for ValidateMsg()

--- common/models/events.go
+++ common/models/events.go
@@ -1,7 +1,5 @@
 package models
 
-import "errors"
-
 const (
 	// Catch fat finger mistakes where an event type is not equal to it's intended event type
 	// ex: a CreateListMsg should never have an EventType "UpdateListEvent"
@@ -24,33 +22,4 @@ const (
 
 type Event interface {
 	IsReadyToBeSaved() error
-}
-
-// ValidateEvent examples the fields that every event needs and throws an error if they're blank
-func ValidateMsg(c *Client, u *User, e *Msg, s *Server) error {
-	if len(c.ID) == 0 {
-		return errors.New(MissingClientIDError)
-	}
-	if len(c.DeviceType) == 0 {
-		return errors.New(MissingClientDeviceTypeError)
-	}
-	if len(u.ID) == 0 {
-		return errors.New(MissingUserIDError)
-	}
-	if len(e.Method) == 0 {
-		return errors.New(MissingMsgMethodError)
-	}
-	if len(e.Resource) == 0 {
-		return errors.New(MissingMsgResourceError)
-	}
-	if len(e.ID) == 0 {
-		return errors.New(MissingEventIDError)
-	}
-	if len(s.Hostname) == 0 {
-		return errors.New(MissingServerHostnameError)
-	}
-	if len(s.IPAddress) == 0 {
-		return errors.New(MissingServerIPAddressError)
-	}
-	return nil
 }
--- common/models/models.go
+++ common/models/models.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Server struct {
 	Hostname  string `json:"hostname"`
@@ -30,4 +33,33 @@ type Msg struct {
 	Method   string `json:"method"`
 	Resource string `json:"resource"`
 	ID       string `json:"id"`
+}
+
+// ValidateEvent examples the fields that every event needs and throws an error if they're blank
+func ValidateMsg(c *Client, u *User, e *Msg, s *Server) error {
+	if len(c.ID) == 0 {
+		return errors.New(MissingClientIDError)
+	}
+	if len(c.DeviceType) == 0 {
+		return errors.New(MissingClientDeviceTypeError)
+	}
+	if len(u.ID) == 0 {
+		return errors.New(MissingUserIDError)
+	}
+	if len(e.Method) == 0 {
+		return errors.New(MissingMsgMethodError)
+	}
+	if len(e.Resource) == 0 {
+		return errors.New(MissingMsgResourceError)
+	}
+	if len(e.ID) == 0 {
+		return errors.New(MissingEventIDError)
+	}
+	if len(s.Hostname) == 0 {
+		return errors.New(MissingServerHostnameError)
+	}
+	if len(s.IPAddress) == 0 {
+		return errors.New(MissingServerIPAddressError)
+	}
+	return nil
 }
