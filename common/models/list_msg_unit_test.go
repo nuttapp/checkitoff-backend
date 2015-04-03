f011b7380d6fe918bd04ecdb140c39e0e8878474
Greg Ingelmo <git@ingelmo.org>
2015-04-02 22:05:03 -0400

    Add unit test for deserializing CreateListMsg json

--- common/models/list_msg_unit_test.go
+++ common/models/list_msg_unit_test.go
@@ -6,6 +6,51 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func Test_DeserializeCreateListMsg_unit(t *testing.T) {
+	Convey("DeserializeCreateListMsg()", t, func() {
+		var createListEventJSON = []byte(`
+			{
+				"method":  "create",
+				"resource": "list",
+				"server": {
+					"hostname": "localhost",
+					"ipAddress": "127.0.0.1"
+				},
+				"client": {
+					"id": "C0085E97-9BCB-4BAA-9A1A-C5AFF37FF433",
+					"deviceType": "iPhone",
+					"osVersion": "7,2"
+				},
+				"user": {
+					"id": "0C6B2206-CAD1-4F9C-BE17-74977A257877",
+					"name": "Greg"
+				},
+				"data": {
+					"id": "12345",
+					"title": "Trader Joes"
+				}
+			}`)
+
+		Convey("Should deserialize json into CreateListMsg", func() {
+			msg, err := DeserializeCreateListMsg(createListEventJSON)
+
+			So(err, ShouldBeNil)
+			So(msg, ShouldNotBeNil)
+			So(msg.Method, ShouldEqual, "create")
+			So(msg.Resource, ShouldEqual, "list")
+			So(msg.Server.Hostname, ShouldEqual, "localhost")
+			So(msg.Server.IPAddress, ShouldEqual, "127.0.0.1")
+			So(msg.Client.ID, ShouldEqual, "C0085E97-9BCB-4BAA-9A1A-C5AFF37FF433")
+			So(msg.Client.DeviceType, ShouldEqual, "iPhone")
+			So(msg.Client.OsVersion, ShouldEqual, "7,2")
+			So(msg.User.ID, ShouldEqual, "0C6B2206-CAD1-4F9C-BE17-74977A257877")
+			So(msg.User.Name, ShouldEqual, "Greg")
+			So(msg.Data.ID, ShouldEqual, "12345")
+			So(msg.Data.Title, ShouldEqual, "Trader Joes")
+		})
+	})
+}
+
 func Test_CreateListMsg_unit(t *testing.T) {
 
 	Convey("NewCreateListMsg()", t, func() {
