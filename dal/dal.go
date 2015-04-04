5160f8d008fffb6e281ead896190396d42256caf
Greg Ingelmo <git@ingelmo.org>
2015-04-04 01:15:38 -0400

    Make DAL into re-usable struct

--- dal/dal.go
+++ dal/dal.go
@@ -14,48 +14,78 @@ type DALCfg struct {
 	Keyspace    string
 	Consistency gocql.Consistency
 }
+
 type DAL struct {
-	Cfg DALCfg
+	Cfg     DALCfg
+	session *gocql.Session
 }
 
-func (d *DAL) SaveCreateListMsg(cle *m.ListMsg) error {
+func NewDAL(cfg DALCfg) (*DAL, error) {
+	d := &DAL{
+		Cfg: cfg,
+	}
+	err := d.initSession()
+	if err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+func (d *DAL) initSession() error {
 	if len(d.Cfg.Hosts) == 0 {
 		return errors.New("No hosts were provided for cassandra cluster")
 	}
 	if len(d.Cfg.Keyspace) == 0 {
 		return errors.New("No keyspace was provided")
 	}
-	cluster := gocql.NewCluster(d.Cfg.Hosts...)
-	cluster.Keyspace = d.Cfg.Keyspace
-	cluster.Consistency = d.Cfg.Consistency
-	session, _ := cluster.CreateSession()
-	defer session.Close()
 
+	if d == nil {
+		cluster := gocql.NewCluster(d.Cfg.Hosts...)
+		cluster.Keyspace = d.Cfg.Keyspace
+		cluster.Consistency = d.Cfg.Consistency
+
+		session, err := cluster.CreateSession()
+		if err != nil {
+			return err
+		}
+		d.session = session
+	}
+	return nil
+}
+
+func (d *DAL) SaveCreateListMsg(msg *m.ListMsg) error {
+	err := d.initSession()
+	if err != nil {
+		return err
+	}
 	createdAt := gocql.TimeUUID()
 	updatedAt := gocql.TimeUUID()
 
-	insertList := session.Query(`INSERT INTO list (list_id, title, created_at, updated_at, users) VALUES (?, ?, ?, ?, ?)`,
-		cle.Data.ID, cle.Data.Title, createdAt, updatedAt, []string{cle.User.ID})
-	err := insertList.Exec()
+	insertList := d.session.Query(
+		`INSERT INTO list (list_id, title, created_at, updated_at, users) VALUES (?, ?, ?, ?, ?)`,
+		msg.Data.ID, msg.Data.Title, createdAt, updatedAt, []string{msg.User.ID})
+	err = insertList.Exec()
 	if err != nil {
 		return err
 	}
 
-	b, err := json.Marshal(cle)
+	b, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
 
-	msgType := fmt.Sprintf("%s-%s", cle.Method, cle.Resource)
-	insertListMsg := session.Query(`INSERT INTO list_event (list_id, user_id, event_id, event_type, data) VALUES (?, ?, ?, ?, ?)`,
-		cle.Data.ID, cle.User.ID, cle.ID, msgType, b)
+	msgType := fmt.Sprintf("%s-%s", msg.Method, msg.Resource)
+	insertListMsg := d.session.Query(
+		`INSERT INTO list_event (list_id, user_id, event_id, event_type, data) VALUES (?, ?, ?, ?, ?)`,
+		msg.Data.ID, msg.User.ID, msg.ID, msgType, b)
 	err = insertListMsg.Exec()
 	if err != nil {
 		return err
 	}
 
-	insertUserTimeline := session.Query(`INSERT INTO user_timeline (user_id, event_id, event_type, data) VALUES (?, ?, ?, ?)`,
-		cle.User.ID, cle.ID, msgType, b)
+	insertUserTimeline := d.session.Query(
+		`INSERT INTO user_timeline (user_id, event_id, event_type, data) VALUES (?, ?, ?, ?)`,
+		msg.User.ID, msg.ID, msgType, b)
 	err = insertUserTimeline.Exec()
 	if err != nil {
 		return err
